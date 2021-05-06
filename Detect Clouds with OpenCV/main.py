b38a5506f93b553084325caaad3866f6f74a8b36
JohannLULW <70780063+JohannLULW@users.noreply.github.com>
2021-05-06 23:45:55 +0200

    Update main.py

--- Detect Clouds with OpenCV/main.py
+++ Detect Clouds with OpenCV/main.py
@@ -1,55 +1,74 @@
 #import cv2     $ pip install opencv-python
 #import numpy   $ pip install numpy
-#import argparse (vorinstalliert)
+#import argparse (preinstalled)
 import cv2
 import numpy as np
 import argparse
 
-#percentage calculator
-def percentage(percent, whole):
-  return (percent * 100) / whole
+if __name__ == "__main__":
+    #percentage calculator
+    def percentage(percent, whole):
+        return (percent * 100) / whole
 
-#--image command
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="path to the input image")
-args = vars(ap.parse_args())
+    #text template
+    def text(pos1, pos2, text):
+        position = (pos1, pos2)
+        cv2.putText(
+        erosion, #numpy array on which text is written
+        text, #text
+        position, #position at which writing has to start
+        cv2.FONT_HERSHEY_SIMPLEX, #font family
+        1, #font size
+        (209, 80, 0, 255), #font color
+        3) #font stroke
 
-#set image
-img = cv2.imread(args["image"], cv2.IMREAD_UNCHANGED)
+    #--image command
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", required=True, help="path to the input image")
+    args = vars(ap.parse_args())
 
-#get reselution
-dimensions = img.shape
-height = img.shape[0]
-width = img.shape[1]
-reselution = height * width
+    #set image
+    img = cv2.imread(args["image"], cv2.IMREAD_UNCHANGED)
 
-#get clouds
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-ret, thresh = cv2.threshold(gray, 250, 255, cv2.THRESH_BINARY)
+    #get reselution
+    dimensions = img.shape
+    height = img.shape[0]
+    width = img.shape[1]
 
-#paint clouds green 
-img[thresh == 255] = 124,252,0
+    #calculate pixel
+    reselution = height * width
 
-#get cloud pixel
-px = np.sum(gray >= 250)
+    #resize reselution
+    if width > 1024:
+        showheight =  height / 2
+        showwidth = width / 2
+    else:
+        showheight =  height
+        showwidth = width
 
-#stuff
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-erosion = cv2.erode(img, kernel, iterations = 1)
+    #get clouds
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray, 250, 255, cv2.THRESH_BINARY)
 
-#write text
-position = (10,50)
-cv2.putText(
-     erosion, #numpy array on which text is written
-     "Clouds: " + str(percentage(px, reselution).round(2)) + "%", #text
-     position, #position at which writing has to start
-     cv2.FONT_HERSHEY_SIMPLEX, #font family
-     1, #font size
-     (209, 80, 0, 255), #font color
-     3) #font stroke
+    #paint clouds green 
+    img[thresh == 255] = 124,252,0
 
-#open image
-cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-cv2.imshow("image", erosion)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    #get cloud pixel
+    px = np.sum(gray >= 250)
+
+    #stuff
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    erosion = cv2.erode(img, kernel, iterations = 1)
+
+    #write text
+    text(10, 50, "Clouds: " + str(percentage(px, reselution).round(2)) + "%")
+    text(10, 90, "Reselution: " + str(width) + " * " + str(height))
+    if width != showheight:
+        text(10, 130, "Resized Reselution: " + str(int(showwidth)) + " * " + str(int(showheight)))
+
+    #open image
+    cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('image', int(showwidth), int(showheight))
+    cv2.imshow("image", erosion)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
